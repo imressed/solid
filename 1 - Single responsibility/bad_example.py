a1d264785c7483f3f5f1cca9614ac3ab0eacc840
imressed <imressed@gmail.com>
2015-04-14 14:48:46 +0300

    add single responsibility example

---
+++ 1 - Single responsibility/bad_example.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+__author__ = 'imressed'
+
+
+class Order:
+    """
+    This class solve 3 different tasks: work with db,
+    work with items, items displaying. It should be separated
+    to 3 different classes.
+    """
+    def get_item(self):
+        pass
+    def set_item(self):
+        pass
+    def delete_item(self):
+        pass
+
+    def show_items(self):
+        pass
+    def print_items(self):
+        pass
+
+    def load(self):
+        pass
+    def save(self):
+        pass
+    def update(self):
+        pass
+    def delete(self):
+        pass
---
+++ 1 - Single responsibility/good_example.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+__author__ = 'imressed'
+
+
+class Order:
+    def get_item(self):
+        pass
+
+    def set_item(self):
+        pass
+
+    def delete_item(self):
+        pass
+
+
+class OrderRepository:
+    def load(self):
+        pass
+
+    def save(self):
+        pass
+
+    def update(self):
+        pass
+
+    def delete(self):
+        pass
+
+
+class OrderViewer:
+    def show_items(self):
+        pass
+
+    def print_items(self):
+        pass
